380bd9093f30c4f53fa5ea6f93089776df53db73
HaosenHuang <88010345+HaosenHuang@users.noreply.github.com>
2022-05-20 19:12:17 +1000

    Add files via upload

---
+++ 8.1C/app/src/main/java/com/example/Task81C/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.Task81C;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        EditText usernameEditText = findViewById(R.id.usernameInput);
+        EditText passwordEditText = findViewById(R.id.passwordInput);
+        Button signupButton = findViewById(R.id.signUpButton);
+        Button loginButton = findViewById(R.id.loginButton);
+        DatabaseHelper db = new DatabaseHelper(this);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+            }
+        });
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                boolean res = db.fetchUser(username, password);
+                int userID = db.getUserID(username);
+                if(res) {
+                    Toast.makeText(MainActivity.this, "Succeeded", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this, PlayerInterface.class);
+                    intent.putExtra("userID", userID);
+                    startActivity(intent);
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
---
+++ 8.1C/app/src/main/java/com/example/Task81C/YoutubeConfig.java
@@ -0,0 +1,13 @@
+package com.example.Task81C;
+
+public class YoutubeConfig {
+
+    public YoutubeConfig() {
+    }
+
+    private static final String API_KEY = "AIzaSyBlDXwTu-URlRYbx8avORU5hM4pQ_He4LU";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
